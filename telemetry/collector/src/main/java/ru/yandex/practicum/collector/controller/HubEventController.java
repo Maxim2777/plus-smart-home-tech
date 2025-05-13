7d59b74e6c8a0d6e7048a81aae9d0901141c2458
2777maxim <2777maxim@mail.ru>
2025-05-13 14:58:14 +0300

    Добавлен отлов ошибок

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/controller/HubEventController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/controller/HubEventController.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.collector.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.collector.model.hub.HubEvent;
 import ru.yandex.practicum.collector.service.HubEventService;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/events/hubs")
@@ -14,9 +16,13 @@ public class HubEventController {
     private final HubEventService hubEventService;
 
     @PostMapping
-    public ResponseEntity<Void> collectHubEvent(@RequestBody HubEvent event) {
-        hubEventService.processEvent(event);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<String> collectHubEvent(@RequestBody HubEvent event) {
+        try {
+            hubEventService.processEvent(event);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            log.error("❌ Ошибка в /events/hubs при обработке события: {}", event, e);
+            return ResponseEntity.internalServerError().body("Ошибка: " + e.getClass().getSimpleName() + ": " + e.getMessage());
+        }
     }
 }
-
--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/controller/SensorEventController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/controller/SensorEventController.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.collector.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.collector.service.SensorEventService;
 import ru.yandex.practicum.collector.model.SensorEvent;
+import ru.yandex.practicum.collector.service.SensorEventService;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/events/sensors")
@@ -14,9 +16,13 @@ public class SensorEventController {
     private final SensorEventService sensorEventService;
 
     @PostMapping
-    public ResponseEntity<Void> collectSensorEvent(@RequestBody SensorEvent event) {
-        sensorEventService.processEvent(event);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<String> collectSensorEvent(@RequestBody SensorEvent event) {
+        try {
+            sensorEventService.processEvent(event);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            log.error("❌ Ошибка в /events/sensors при обработке события: {}", event, e);
+            return ResponseEntity.internalServerError().body("Ошибка: " + e.getClass().getSimpleName() + ": " + e.getMessage());
+        }
     }
 }
-
