b1331c09e8573c2778c29f3edd5c913b8ad4c151
2777maxim <2777maxim@mail.ru>
2025-05-14 14:34:44 +0300

    изменение config

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/config/KafkaConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/config/KafkaConfig.java
@@ -5,8 +5,10 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.apache.avro.specific.SpecificRecord;
 import org.springframework.kafka.core.*;
+
+import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,18 +16,22 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
-    @Bean
-    public ProducerFactory<String, SpecificRecord> producerFactory() {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        config.put("schema.registry.url", "http://localhost:8081");
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-        return new DefaultKafkaProducerFactory<>(config);
+    private Map<String, Object> baseConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put("schema.registry.url", "http://localhost:8081");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        return props;
     }
 
     @Bean
-    public KafkaTemplate<String, SpecificRecord> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+    public KafkaTemplate<String, SensorEventAvro> sensorEventKafkaTemplate() {
+        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(baseConfigs()));
+    }
+
+    @Bean
+    public KafkaTemplate<String, HubEventAvro> hubEventKafkaTemplate() {
+        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(baseConfigs()));
     }
 }
