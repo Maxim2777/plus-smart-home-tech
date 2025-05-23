701b3c7d5f91cfda37e93bff22e769db732480d4
2777maxim <2777maxim@mail.ru>
2025-05-23 22:30:04 +0300

    правка

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/config/KafkaClientConfig.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/config/KafkaClientConfig.java
@@ -12,6 +12,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 
 import java.util.Properties;
 
@@ -23,13 +24,11 @@ public class KafkaClientConfig {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "sensor-aggregator");
-
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
-
         props.put("schema.registry.url", "http://localhost:8081");
         props.put("specific.avro.reader", "true");
-
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         return new KafkaConsumer<>(props);
     }
 
@@ -37,12 +36,19 @@ public class KafkaClientConfig {
     public Producer<String, SensorEventAvro> kafkaProducer() {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
-
         props.put("schema.registry.url", "http://localhost:8081");
+        return new KafkaProducer<>(props);
+    }
 
+    @Bean
+    public Producer<String, SensorsSnapshotAvro> kafkaSnapshotProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
+        props.put("schema.registry.url", "http://localhost:8081");
         return new KafkaProducer<>(props);
     }
 }
