9863cac86849a95b71a7a5eb1048c62b1c8dcb14
2777maxim <2777maxim@mail.ru>
2025-05-23 23:16:42 +0300

    правка

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/config/KafkaClientConfig.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/config/KafkaClientConfig.java
@@ -1,16 +1,10 @@
 package ru.yandex.practicum.aggregator.config;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.yandex.practicum.aggregator.deserializer.SensorEventDeserializer;
-import ru.yandex.practicum.aggregator.serializer.GeneralAvroSerializer;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 
@@ -19,22 +13,23 @@ import java.util.Properties;
 @Configuration
 public class KafkaClientConfig {
 
+    private final KafkaPropertiesConfig kafkaProperties;
+
+    public KafkaClientConfig(KafkaPropertiesConfig kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
+
     @Bean
     public KafkaConsumer<String, SensorEventAvro> kafkaConsumer() {
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "sensor-aggregator");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class);
+        props.putAll(kafkaProperties.getConsumer());
         return new KafkaConsumer<>(props);
     }
 
     @Bean
     public Producer<String, SensorsSnapshotAvro> kafkaProducer() {
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class.getName());
+        props.putAll(kafkaProperties.getProducer());
         return new KafkaProducer<>(props);
     }
 }
