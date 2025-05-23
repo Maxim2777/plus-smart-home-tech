0d1c2c4fd47f535c02ee2a6a0d3043029402b2fb
2777maxim <2777maxim@mail.ru>
2025-05-23 15:57:56 +0300

    kafka - переход на spring kafka

---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregation/config/KafkaConfig.java
@@ -0,0 +1,32 @@
+package ru.yandex.practicum.telemetry.aggregation.config;
+
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.*;
+import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ProducerFactory<String, SensorsSnapshotAvro> producerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        props.put("schema.registry.url", "http://localhost:8081");
+
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, SensorsSnapshotAvro> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
--- telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregation/runner/AggregationStarter.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregation/runner/AggregationStarter.java
@@ -3,11 +3,10 @@ package ru.yandex.practicum.telemetry.aggregation.runner;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.*;
-import org.apache.kafka.clients.producer.*;
-import org.apache.kafka.common.errors.WakeupException;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 import ru.yandex.practicum.telemetry.aggregation.service.AggregationService;
 
 import java.time.Duration;
@@ -21,6 +20,7 @@ import java.util.Properties;
 public class AggregationStarter {
 
     private final AggregationService aggregationService;
+    private final KafkaTemplate<String, SensorsSnapshotAvro> kafkaTemplate;
 
     private final String SENSOR_TOPIC = "telemetry.sensors.v1";
     private final String SNAPSHOT_TOPIC = "telemetry.snapshots.v1";
@@ -35,43 +35,24 @@ public class AggregationStarter {
         consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "ru.yandex.practicum.telemetry.aggregation.serialization.SensorEventDeserializer");
         consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        try (KafkaConsumer<String, SensorEventAvro> consumer = new KafkaConsumer<>(consumerProps);
-             KafkaProducer<String, SensorsSnapshotAvro> producer = createProducer()) {
-
+        try (KafkaConsumer<String, SensorEventAvro> consumer = new KafkaConsumer<>(consumerProps)) {
             consumer.subscribe(List.of(SENSOR_TOPIC));
 
             while (true) {
                 ConsumerRecords<String, SensorEventAvro> records = consumer.poll(Duration.ofMillis(500));
                 for (ConsumerRecord<String, SensorEventAvro> record : records) {
                     SensorEventAvro event = record.value();
-
                     Optional<SensorsSnapshotAvro> snapshotOpt = aggregationService.updateState(event);
                     snapshotOpt.ifPresent(snapshot -> {
-                        ProducerRecord<String, SensorsSnapshotAvro> msg =
-                                new ProducerRecord<>(SNAPSHOT_TOPIC, snapshot.getHubId(), snapshot);
-                        producer.send(msg);
+                        kafkaTemplate.send(SNAPSHOT_TOPIC, snapshot.getHubId(), snapshot);
                         log.debug("📦 Отправлен снапшот хаба {} в Kafka", snapshot.getHubId());
                     });
                 }
-
-                // фиксируем смещения
                 consumer.commitSync();
-                // сбрасываем буфер продюсера
-                producer.flush();
             }
-        } catch (WakeupException ignored) {
-            log.info("⛔ Получен сигнал завершения (WakeupException)");
+
         } catch (Exception e) {
             log.error("💥 Ошибка во время обработки событий", e);
         }
-    }
-
-    private KafkaProducer<String, SensorsSnapshotAvro> createProducer() {
-        Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroSerializer");
-        props.put("schema.registry.url", "http://localhost:8081");
-        return new KafkaProducer<>(props);
     }
 }
