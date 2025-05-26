a57becd1432be00255ac8c83f7360c9f9a42b975
2777maxim <2777maxim@mail.ru>
2025-05-27 01:53:39 +0300

    поправил KafkaConsumerConfig

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/config/KafkaConsumerConfig.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/config/KafkaConsumerConfig.java
@@ -6,7 +6,9 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 import ru.yandex.practicum.telemetry.analyzer.deserializer.HubEventDeserializer;
+import ru.yandex.practicum.telemetry.analyzer.deserializer.SensorsSnapshotDeserializer;
 
 import java.util.Properties;
 
@@ -20,6 +22,19 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "analyzer-hub-events");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, HubEventDeserializer.class);
+        return new KafkaConsumer<>(props);
+    }
+
+
+    @Bean
+    public KafkaConsumer<String, SensorsSnapshotAvro> snapshotKafkaConsumer() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "analyzer-snapshot-events");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorsSnapshotDeserializer.class.getName());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
         return new KafkaConsumer<>(props);
     }
 }
