d6ec3cdbf817e0ec10b98ab20162a5d1425f3763
2777maxim <2777maxim@mail.ru>
2025-05-28 02:39:52 +0300

    удаление закомментированных классов

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/repository/ScenarioActionRepository.java
+++
@@ -1,8 +0,0 @@
-/*package ru.yandex.practicum.telemetry.analyzer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.yandex.practicum.telemetry.analyzer.model.ScenarioAction;
-import ru.yandex.practicum.telemetry.analyzer.model.ScenarioActionId;
-
-public interface ScenarioActionRepository extends JpaRepository<ScenarioAction, ScenarioActionId> {
-}*/
