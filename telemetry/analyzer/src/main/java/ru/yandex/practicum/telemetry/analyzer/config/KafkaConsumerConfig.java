606892d992341116e26d51b1387ae8c6f1e9de3a
2777maxim <2777maxim@mail.ru>
2025-05-28 04:05:53 +0300

    возврат к рабочему состоянию

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/config/KafkaConsumerConfig.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/config/KafkaConsumerConfig.java
@@ -2,10 +2,13 @@ package ru.yandex.practicum.telemetry.analyzer.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
+import ru.yandex.practicum.telemetry.analyzer.deserializer.HubEventDeserializer;
+import ru.yandex.practicum.telemetry.analyzer.deserializer.SensorsSnapshotDeserializer;
 
 import java.util.Properties;
 
@@ -13,26 +16,25 @@ import java.util.Properties;
 public class KafkaConsumerConfig {
 
     @Bean
-    public KafkaConsumer<String, SensorsSnapshotAvro> snapshotKafkaConsumer(SnapshotKafkaProperties props) {
-        Properties kafkaProps = new Properties();
-        kafkaProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, props.getBootstrapServers());
-        kafkaProps.put(ConsumerConfig.GROUP_ID_CONFIG, props.getGroupId());
-        kafkaProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, props.getAutoOffsetReset());
-        kafkaProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, props.getKeyDeserializer());
-        kafkaProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, props.getValueDeserializer());
-
-        return new KafkaConsumer<>(kafkaProps);
+    public KafkaConsumer<String, HubEventAvro> kafkaConsumer() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "analyzer-hub-events");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, HubEventDeserializer.class);
+        return new KafkaConsumer<>(props);
     }
 
-    @Bean
-    public KafkaConsumer<String, HubEventAvro> hubEventKafkaConsumer(HubKafkaProperties props) {
-        Properties kafkaProps = new Properties();
-        kafkaProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, props.getBootstrapServers());
-        kafkaProps.put(ConsumerConfig.GROUP_ID_CONFIG, props.getGroupId());
-        kafkaProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, props.getAutoOffsetReset());
-        kafkaProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, props.getKeyDeserializer());
-        kafkaProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, props.getValueDeserializer());
 
-        return new KafkaConsumer<>(kafkaProps);
+    @Bean
+    public KafkaConsumer<String, SensorsSnapshotAvro> snapshotKafkaConsumer() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "analyzer-snapshot-events");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorsSnapshotDeserializer.class.getName());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        return new KafkaConsumer<>(props);
     }
 }
--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/service/grpc/HubRouterClient.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/service/grpc/HubRouterClient.java
@@ -1,18 +1,18 @@
 package ru.yandex.practicum.telemetry.analyzer.service.grpc;
 
 import io.grpc.StatusRuntimeException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.grpc.telemetry.event.DeviceActionRequest;
 import ru.yandex.practicum.grpc.telemetry.hubrouter.HubRouterControllerGrpc;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class HubRouterClient {
 
-    @GrpcClient("hub-router")
-    private HubRouterControllerGrpc.HubRouterControllerBlockingStub stub;
+    private final HubRouterControllerGrpc.HubRouterControllerBlockingStub stub;
 
     public void sendAction(DeviceActionRequest request) {
         try {
